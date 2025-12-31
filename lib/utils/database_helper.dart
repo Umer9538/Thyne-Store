eebe5ee2c8b05820a150bc06623fb183f3302b34
Muhammad Umer <muhammadumer9538@gmail.com>
2025-12-31 15:28:15 +0500

    cleanup: Remove all unused files and directories
    Removed unused utils:
    - database_helper (all variants) - not imported anywhere
    - mock_data.dart - not used
    
    Removed unused models:
    - validation_rules.dart - not imported
    - invoice.dart - only used by removed invoice screen
    
    Removed unused services:
    - gemini_service.dart - not imported
    - csv_export (all variants) - only used by removed invoice screen
    
    Removed unused screens:
    - phone_login_screen.dart
    - guest_conversion_screen.dart
    - customers_management_screen.dart
    - quality_tiers_screen.dart
    - invoice_management_screen.dart
    - thyne_main_navigation.dart
    - ai_create_section.dart
    - commerce_section.dart
    
    Removed unused widgets:
    - customization_preview_widget.dart
    - responsive_layout.dart
    - brand_showcase_widget.dart
    - overlay_page.dart
    - common widgets folder (all files) - created but never integrated
    
    Removed empty directories:
    - data/repositories
    - views/navigation
    - views/commerce
    - views/admin/customers
    - views/admin/invoices
    - widgets/common
    
    Authored-By: Muhammad Umer

--- lib/utils/database_helper.dart
+++
@@ -1,5 +0,0 @@
-/// Database helper that conditionally imports the correct implementation
-/// based on the platform (web or mobile)
-export 'database_helper_stub.dart'
-    if (dart.library.io) 'database_helper_mobile.dart'
-    if (dart.library.html) 'database_helper_web.dart';
